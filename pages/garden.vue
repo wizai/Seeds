d9d9cf1961871c6b7a74baaa49ce496dcfcc6605
wizai <william.nisole@gmail.com>
2020-06-15 09:30:07 +0200

    Start project

---
+++ pages/garden.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "garden"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/plant.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "plant"
+    }
+</script>
+
+<style scoped>
+
+</style>
